ef6089659dd02946337135beb3adc4d849284f8d
sub-prime <122296954+lakem0de@users.noreply.github.com>
2024-03-04 23:46:33 +0200

    Add "playGame" function

--- js.js
+++ js.js
@@ -19,3 +19,26 @@ function playRound(playerSelection, computerSelection) {
         return "You lose!";
     }
 }
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose Rock, Paper, or Scissors!", "").toLowerCase();
+        const computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(' * ROUND ' + (i+1) + ' * ');
+        if (result.includes("win")) {
+            playerScore++;
+            console.log(`Your ${playerSelection} beats computer's ${computerSelection}!`);
+         } else if (result.includes("lose")) {
+            computerScore++;
+            console.log(`Your ${playerSelection} loses to computer's ${computerSelection}!`);            
+         } else {
+            console.log(`It's a tie between your and computer's ${playerSelection}!`);
+         }
+         console.log(`Score: You - ${playerScore}   Computer - ${computerScore}`);
+    }
+    console.log((playerScore > computerScore) ? "You have won the game!" :
+    (playerScore === computerScore) ? "The game ended in a tie!" : "You have lost the game!");
+}   
+playGame();
